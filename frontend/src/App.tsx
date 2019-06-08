55fb703422d053c754e226ddeff6c75b63d33fec
bdbaraban <375@holbertonschool.com>
2019-06-08 13:25:35 -0700

    Isolated routes into separate folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import { Router, View } from 'react-navi';
 import { makeStyles } from '@material-ui/core';
 import { Styles } from 'jss';
-import routes from './routes';
+import { routes } from './routes';
 
 const useStyles = makeStyles(
   (): Styles => ({
