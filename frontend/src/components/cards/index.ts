627690a2057f977f8b1eeaac52314b0d82d66101
bdbaraban <375@holbertonschool.com>
2019-06-10 00:06:35 -0700

    Update barrel file to reflect latest folder structure

--- frontend/src/components/cards/index.ts
+++ frontend/src/components/cards/index.ts
@@ -1,5 +1,6 @@
-export { NewIdeaCard, NewIdeaDialog, LoginDialog } from './NewIdeaCard';
+export { NewIdeaCard, NewIdeaDialog } from './NewIdeaCard';
 export { default as AboutCard } from './AboutCard';
 export { default as IdeaCard } from './IdeaCard';
 export { default as TagsCard, CheckboxTag } from './TagsCard';
 export { default as UserCard } from './UserCard';
+export { default as SettingsCard } from './SettingsCard';
