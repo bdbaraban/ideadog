90103e4571a11f6e96fb0e66a50fe8e5519deaba
bdbaraban <375@holbertonschool.com>
2019-06-10 00:00:15 -0700

    Update components barrel files to include latest folder components + reorganization

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,13 +1,15 @@
 export { default as Navbar } from './Navbar';
 export { default as NewIdeaFab } from './NewIdeaFab';
 export { default as CopyToClipboard } from './CopyToClipboard';
+export { default as CustomTextField } from './CustomTextField';
+export { LoginDialog, NotLoggedInDialog } from './login';
 export {
   NewIdeaCard,
   AboutCard,
   IdeaCard,
   TagsCard,
   UserCard,
-  NewIdeaDialog,
-  LoginDialog
+  SettingsCard,
+  NewIdeaDialog
 } from './cards';
 export { IdeaGridItem, TagsCardListItem } from './items';
--- frontend/src/grids/index.ts
+++ frontend/src/grids/index.ts
@@ -1,4 +1,7 @@
 export { default as HomeLayout } from './HomeLayout';
 export { default as IdeaLayout } from './IdeaLayout';
+export { default as UserLayout } from './UserLayout';
 export { default as IdeaGrid } from './IdeaGrid';
 export { default as InfoGrid } from './InfoGrid';
+export { default as UserGrid } from './UserGrid';
+export { default as UserIdeaGrid } from './UserIdeaGrid';
