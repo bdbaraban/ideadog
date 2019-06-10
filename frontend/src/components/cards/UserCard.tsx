a0d2962a795a295e727139bbfcb7556e2d380e92
bdbaraban <375@holbertonschool.com>
2019-06-10 00:06:18 -0700

    Update UserCard to work with user stored i route context

--- frontend/src/components/cards/UserCard.tsx
+++ frontend/src/components/cards/UserCard.tsx
@@ -7,10 +7,14 @@ import {
   Theme,
   CardContent,
   Typography,
-  Container
+  Container,
+  Icon,
+  Dialog
 } from '@material-ui/core';
 import { Styles } from 'jss';
-import { useUserStatus } from '../../api';
+import { UserAuth } from '../../api';
+import { LoginDialog } from '../';
+import { Link } from 'react-navi';
 
 const useStyles = makeStyles(
   (theme: Theme): Styles =>
@@ -38,44 +42,91 @@ const useStyles = makeStyles(
       },
       infoContainer: {
         display: 'flex',
-        flexDirection: 'column',
-        padding: 0
-      },
-      infoLine: {
-        display: 'flex',
+        flexDirection: 'row',
         justifyContent: 'space-between',
         padding: 0
+      },
+      infoLeft: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        padding: 0
+      },
+      infoRight: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        padding: 0
+      },
+      infoPieces: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      link: {
+        textDecoration: 'none',
+        color: theme.palette.common.white
       }
     })
 );
 
-const UserCard = (): React.ReactElement => {
-  const classes = useStyles();
-  const [isOnline, setIsOnline] = React.useState<boolean>(useUserStatus());
+interface UserCardProps {
+  user: UserAuth;
+}
 
-  const handleLogin = (): void => {
-    setIsOnline(true);
+const UserCard = ({ user }: UserCardProps): React.ReactElement => {
+  const classes = useStyles();
+
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const toggleOpen = (): void => {
+    setOpen(!open);
   };
 
   return (
     <Card raised={true} className={classes.card}>
-      {isOnline ? (
+      {user.currentUser ? (
         <CardContent className={classes.cardContent}>
-          <Typography className={classes.infoTitle} color="textSecondary">
-            @bdov_
-          </Typography>
           <Container className={classes.infoContainer}>
-            <Container className={classes.infoLine}>
+            <Container className={classes.infoLeft}>
+              <Typography className={classes.infoTitle} color="textSecondary">
+                @{user.currentUser.username}
+              </Typography>
               <Typography color="textSecondary">Ideas:</Typography>
-              <Typography color="textSecondary">7</Typography>
-            </Container>
-            <Container className={classes.infoLine}>
               <Typography color="textSecondary">Brightness:</Typography>
-              <Typography color="textSecondary">87%</Typography>
-            </Container>
-            <Container className={classes.infoLine}>
               <Typography color="textSecondary">Top Tag:</Typography>
-              <Typography color="textSecondary">Dogs</Typography>
+            </Container>
+
+            <Container className={classes.infoRight}>
+              <div className={classes.infoPieces}>
+                <Link
+                  className={classes.link}
+                  href={`/user/${user.currentUser.key}`}
+                >
+                  <Icon fontSize="small" color="inherit">
+                    account_circle
+                  </Icon>
+                </Link>
+                <div className={classes.infoRight}>
+                  <Typography color="textSecondary">7</Typography>
+                </div>
+                <div className={classes.infoRight}>
+                  <Typography color="textSecondary">
+                    {Math.round(
+                      (user.currentUser.upvotes /
+                        (user.currentUser.upvotes +
+                          user.currentUser.downvotes)) *
+                        100
+                    )}
+                    %
+                  </Typography>
+                </div>
+                <div className={classes.infoRight}>
+                  <Typography color="textSecondary">Dogs</Typography>
+                </div>
+              </div>
             </Container>
           </Container>
         </CardContent>
@@ -87,10 +138,13 @@ const UserCard = (): React.ReactElement => {
               color="secondary"
               size="large"
               className={classes.loginButton}
-              onClick={handleLogin}
+              onClick={toggleOpen}
             >
               Log In/Sign Up
             </Button>
+            <Dialog open={open} onClose={toggleOpen}>
+              <LoginDialog user={user} toggleOpen={toggleOpen} />
+            </Dialog>
           </Container>
         </CardContent>
       )}
