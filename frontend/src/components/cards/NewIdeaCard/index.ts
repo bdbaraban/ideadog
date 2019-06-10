1e28c58d419089f5b9365c1748f966f3c927c61c
bdbaraban <375@holbertonschool.com>
2019-06-10 00:04:47 -0700

    Update barrel file to reflect latest folder structure

--- frontend/src/components/cards/NewIdeaCard/index.ts
+++ frontend/src/components/cards/NewIdeaCard/index.ts
@@ -1,6 +1,4 @@
 export { default as NewIdeaCard } from './NewIdeaCard';
-export { default as NewIdeaTextField } from './NewIdeaTextField';
 export { default as NewIdeaDialog } from './NewIdeaDialog';
-export { default as LoginDialog } from './LoginDialog';
 export { default as TagsInputLabel } from './TagsInputLabel';
 export { default as TagsSelect } from './TagsSelect';
