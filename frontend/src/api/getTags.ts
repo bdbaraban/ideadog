ed7dcc77be3af390b8acafb7cd48773295d91635
bdbaraban <375@holbertonschool.com>
2019-06-10 00:03:53 -0700

    Refactor getTags function to make actual calls on API

--- frontend/src/api/getTags.ts
+++ frontend/src/api/getTags.ts
@@ -3,6 +3,10 @@ import { Tag } from './';
 const getTags = async (): Promise<Tag[]> => {
   const response = await fetch('http://localhost:5000/api/tags');
   const data = await response.json();
+
+  if (!data) {
+    throw new Error();
+  }
   return Object.values(data);
 };
 
