6db0ef236e9bc670df4cdaeb9db4a5dbc9e09e60
bdbaraban <375@holbertonschool.com>
2019-06-09 23:59:29 -0700

    Update barrel file to include latest folder components

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,4 +1,7 @@
 export { Title, RoutePromise, LazyImport } from './types';
 export { default as routes } from './routes';
 export { default as idea } from './idea';
-export { default as NotFoundPage } from './NotFoundPage';
+export { default as user } from './user';
+export { default as Layout } from './Layout';
+export { default as ErrorPage } from './ErrorPage';
+export { default as ErrorBoundary } from './ErrorBoundary';
