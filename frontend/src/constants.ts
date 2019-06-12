d2e272379ebbc30827c3f7fc88b4880f17017557
bdbaraban <375@holbertonschool.com>
2019-06-11 21:19:12 -0700

    Upload dedicated constants folder defining common constant months codex

---
+++ frontend/src/constants.ts
@@ -0,0 +1,17 @@
+/**
+ * Common months object codex
+ */
+export const MONTHS: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
