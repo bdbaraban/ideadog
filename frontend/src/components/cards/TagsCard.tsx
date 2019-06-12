5f3e40c2cb437bcb78fd45fd45a2dd2bca144ae8
bdbaraban <375@holbertonschool.com>
2019-06-11 21:55:30 -0700

    Incorporate checkboxTags prop and write documentation comments

--- frontend/src/components/cards/TagsCard.tsx
+++ frontend/src/components/cards/TagsCard.tsx
@@ -5,14 +5,15 @@ import {
   createStyles,
   makeStyles,
   Theme,
-  Typography,
-  MenuList
+  Typography
 } from '@material-ui/core';
 import { Styles } from 'jss';
-import { useNavigation, useCurrentRoute } from 'react-navi';
-import { TagsCardListItem } from '../';
-import { Tag } from '../../api';
+import { CheckboxTag } from '../../types';
+import { TagsMenuList } from '..';
 
+/**
+ * TagsCard component style
+ */
 const useStyles = makeStyles(
   (theme: Theme): Styles =>
     createStyles({
@@ -40,73 +41,19 @@ const useStyles = makeStyles(
     })
 );
 
+/**
+ * TagsCard component prop types
+ */
 interface TagsCardProps {
-  currentTags: string[];
-  allTags: Tag[];
+  // Array of currently-checked tag names
+  checkboxTags: CheckboxTag[];
 }
 
-export interface CheckboxTag {
-  [key: string]: {
-    count: number;
-    checked: boolean;
-  };
-}
-
-const TagsCard = ({
-  currentTags,
-  allTags
-}: TagsCardProps): React.ReactElement => {
+/**
+ * Tag-filtering card component displayed as second item in InfoGrid
+ */
+const TagsCard = ({ checkboxTags }: TagsCardProps): React.ReactElement => {
   const classes = useStyles();
-  const route = useCurrentRoute();
-  const navigation = useNavigation();
-
-  const [checkboxTags, setCheckboxTags] = React.useState<CheckboxTag[]>([]);
-
-  React.useEffect((): void => {
-    setCheckboxTags(
-      allTags.map(
-        (tag: Tag): CheckboxTag => {
-          const name = `${tag.key.charAt(0).toUpperCase()}${tag.key.slice(1)}`;
-          let obj: CheckboxTag = {};
-          obj[name] = {
-            count: tag.count,
-            checked: currentTags.includes(name)
-          };
-          return obj;
-        }
-      )
-    );
-  }, [allTags]);
-
-  const handleToggle = (
-    tag: CheckboxTag,
-    index: number
-  ): (() => void) => (): void => {
-    const checked = [...checkboxTags];
-
-    tag[Object.keys(tag)[0]].checked = !tag[Object.keys(tag)[0]].checked;
-    checked[index] = tag;
-    setCheckboxTags(checked);
-
-    const tags: string = checked.reduce(
-      (query: string, tag: CheckboxTag): string => {
-        if (Object.values(tag)[0].checked) {
-          if (query !== '') {
-            query += ',';
-          }
-          query += Object.keys(tag)[0].toLowerCase();
-        }
-        return query;
-      },
-      ''
-    );
-
-    navigation.navigate(
-      tags.length !== 0
-        ? `${route.url.hostname}?tags=${encodeURIComponent(tags)}`
-        : `${route.url.hostname}`
-    );
-  };
 
   return (
     <Card raised={true} className={classes.card}>
@@ -114,21 +61,10 @@ const TagsCard = ({
         <Typography className={classes.title} color="textSecondary">
           Tags
         </Typography>
-        <MenuList id="tags-menu" className={classes.list}>
-          {checkboxTags.map(
-            (tag: CheckboxTag, index: number): React.ReactElement => (
-              <TagsCardListItem
-                key={Object.keys(tag)[0]}
-                tag={tag}
-                handleToggle={handleToggle}
-                index={index}
-              />
-            )
-          )}
-        </MenuList>
+        <TagsMenuList checkboxTags={checkboxTags} />
       </CardContent>
     </Card>
   );
 };
 
-export default TagsCard;
+export default React.memo(TagsCard);
