6e7194daf5fd1589fbad66c20be8f243383b77ad
bdbaraban <375@holbertonschool.com>
2019-06-11 21:57:17 -0700

    Update barrel file with latest state of folder

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,5 +1,7 @@
-export { Idea, Tag, User } from './types';
+// API barrel file
 export { default as getIdeas } from './getIdeas';
 export { default as getIdea } from './getIdea';
 export { default as getTags } from './getTags';
-export { default as UserAuth } from './UserAuth';
+export { default as getUser } from './getUser';
+export { default as getUserIdeas } from './getUserIdeas';
+export { default as UserSession } from './UserSession';
