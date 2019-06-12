94c909401562786c03ee8ab60ddc9b0d160be8b9
bdbaraban <375@holbertonschool.com>
2019-06-11 21:24:58 -0700

    Update barrel file to reflect latest updated state of folder

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,7 +1,9 @@
-export { Title, RoutePromise, LazyImport } from './types';
-export { default as routes } from './routes';
+// Routes barrel file
+export { LazyImport, RouteContext, RoutePromise, Title } from './types';
+export { default as home } from './home';
 export { default as idea } from './idea';
 export { default as user } from './user';
 export { default as Layout } from './Layout';
 export { default as ErrorPage } from './ErrorPage';
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as setCheckboxTags } from './setCheckboxTags';
