f5ec0e317b290d4264ad815c5604f7adbee54fe2
bdbaraban <375@holbertonschool.com>
2019-06-11 21:52:50 -0700

    Write module-level comment at top

--- frontend/src/components/cards/index.ts
+++ frontend/src/components/cards/index.ts
@@ -1,6 +1,7 @@
-export { NewIdeaCard, NewIdeaDialog } from './NewIdeaCard';
+// Cards barrel file
+export { default as NewIdeaCard } from './NewIdeaCard';
 export { default as AboutCard } from './AboutCard';
 export { default as IdeaCard } from './IdeaCard';
-export { default as TagsCard, CheckboxTag } from './TagsCard';
+export { default as TagsCard } from './TagsCard';
 export { default as UserCard } from './UserCard';
 export { default as SettingsCard } from './SettingsCard';
