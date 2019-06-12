6f97ab3eb4bda80067141fe2766bde632f74d854
bdbaraban <375@holbertonschool.com>
2019-06-11 21:24:31 -0700

    Incorporate new checkboxTags prop and write documentation comments

--- frontend/src/routes/idea.tsx
+++ frontend/src/routes/idea.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { mount, NaviRequest, route } from 'navi';
-import { Idea, getIdea } from '../api';
+import { getIdea, getTags } from '../api';
 import { Navbar } from '../components';
 import { IdeaLayout } from '../grids';
-import { RoutePromise } from './';
+import { CheckboxTag, Idea } from '../types';
+import { RouteContext, RoutePromise, setCheckboxTags } from '.';
 
+/**
+ * Individual idea page
+ */
 export default mount({
   '/:key': route(
-    async (req: NaviRequest<object>): Promise<RoutePromise> => {
-      const key: string = req.params.key;
-      const ideas: Idea[] = await getIdea({ key });
+    async (
+      request: NaviRequest<object>,
+      context: RouteContext
+    ): Promise<RoutePromise> => {
+      // Idea key
+      const { key } = request.params;
+
+      // Get idea with key `key`
+      const idea: Idea = await getIdea({ key });
+
+      // Get all available tags
+      const allTags = await getTags();
+
+      // Set checkbox tags based on allTags and query tags
+      const checkboxTags: CheckboxTag[] = setCheckboxTags(undefined, allTags);
 
       return {
         title: 'IdeaDog - Idea',
         view: (
           <div>
-            <Navbar sort={'home'} />
-            <IdeaLayout idea={ideas[0]} />
+            <Navbar
+              sort={'home'}
+              user={context.user}
+              checkboxTags={checkboxTags}
+            />
+            <IdeaLayout idea={idea} />
           </div>
         )
       };
