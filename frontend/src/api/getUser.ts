fd25077b651631c713b99d1710c2683afca76a7d
bdbaraban <375@holbertonschool.com>
2019-06-13 21:09:24 -0700

    Implement fetching of users from API using new backend authorization process

--- frontend/src/api/getUser.ts
+++ frontend/src/api/getUser.ts
@@ -1,29 +1,18 @@
-import { NotFoundError } from 'navi';
 import { User } from '../types';
-
-/**
- * getUser parameter types
- */
-interface SearchParameters {
-  // The key of the user to fetch
-  key: string;
-}
 
 /**
  * Fetches a single user from the IdeaDog API
  */
-const getUser = async ({ key }: SearchParameters): Promise<User> => {
-  // const response = await fetch(`/api/user/${key}`);
-  // const data: Idea[] = await response.json();
+const getUser = async (bearer: string): Promise<User> => {
+  const response = await fetch('/api/user', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${bearer}`
+    }
+  });
 
-  /*
-  if (!data) {
-    throw new NotFoundError(`Idea with ID '${key}' does not exist.`);
-  }
-  return data;
-  */
-
-  return {};
+  const data: User[] = await response.json();
+  return data[0];
 };
 
 export default getUser;
