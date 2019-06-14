2bce6b17309dca761f739aa6a1c079702e63025e
bdbaraban <375@holbertonschool.com>
2019-06-13 21:18:59 -0700

    Uncomment implemented function fetching ideas for a given user

--- frontend/src/api/getUserIdeas.ts
+++ frontend/src/api/getUserIdeas.ts
@@ -13,17 +13,13 @@ interface SearchParameters {
  * Fetches all ideas posted by a given user from the IdeaDog API
  */
 const getUserIdeas = async ({ key }: SearchParameters): Promise<Idea[]> => {
-  // const response = await fetch(`/api/user/${key}/ideas`);
-  // const data: Idea[] = await response.json();
+  const response = await fetch(`/api/user/${key}/ideas`);
+  const data: Idea[] = await response.json();
 
-  /*
   if (!data) {
     throw new NotFoundError(`Idea with ID '${key}' does not exist.`);
   }
   return data;
-  */
-
-  return [];
 };
 
 export default getUserIdeas;
