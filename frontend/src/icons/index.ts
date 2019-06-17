bef11be628b807435a9a0b74200409e818f5d248
bdbaraban <375@holbertonschool.com>
2019-06-17 15:00:21 -0700

    Update icons barrel file to include new SVG files

--- frontend/src/icons/index.ts
+++ frontend/src/icons/index.ts
@@ -1,3 +1,5 @@
 // Icons barrel file
 export { default as HappyTully } from './HappyTully';
+export { default as BrightHappyTully } from './BrightHappyTully';
 export { default as SadTully } from './SadTully';
+export { default as BrightSadTully } from './BrightSadTully';
