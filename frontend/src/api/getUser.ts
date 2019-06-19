59f628434723769b80474be1ee7c277cedf38cb0
bdbaraban <375@holbertonschool.com>
2019-06-18 17:24:57 -0700

    Implement error handling for unmatched users

--- frontend/src/api/getUser.ts
+++ frontend/src/api/getUser.ts
@@ -1,17 +1,18 @@
+import { NotFoundError } from 'navi';
 import { User } from '../types';
 
 /**
  * Fetches a single user from the IdeaDog API
  */
-const getUser = async (bearer: string): Promise<User> => {
-  const response = await fetch('/api/user', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${bearer}`
-    }
+const getUser = async (key: string): Promise<User> => {
+  const response = await fetch(`http://localhost:5000/api/user/${key}`, {
+    method: 'GET'
   });
 
   const data: User[] = await response.json();
+  if (!data) {
+    throw new NotFoundError(`User with ID ${key} does not exist.`);
+  }
   return data[0];
 };
 
