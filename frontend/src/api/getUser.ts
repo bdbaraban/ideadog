15da345cd2c6cf2e3aac0c9f4c90b72c2846da01
bdbaraban <375@holbertonschool.com>
2019-06-29 17:37:18 -0700

    Set both key and bearer parameter as undefined for cleaner logic; write JSdoc style function comments and integrate API constant

--- frontend/src/api/getUser.ts
+++ frontend/src/api/getUser.ts
@@ -1,25 +1,35 @@
 import { NotFoundError } from 'navi';
+import { API } from '../constants';
 import { User } from '../types';
 
 /**
- * Fetch a logged in user from the IdeaDog API.
- * @param bearer {string} - Bearer token of the current logged in user.
+ * Fetch a user from the IdeaDog API.
+ * @param key {string} - The ID of the user to fetch (undefined if fetching logged in user).
+ * @param bearer {string} - Logged in user's bearer token (undefined if not fetching logged in user).
  */
-const getUser = async (key: string | undefined = undefined): Promise<User> => {
-  const route = key
-    ? `http://localhost:5000/api/user/${key}`
-    : 'http://localhost:5000/api/user';
+const getUser = async (
+  key: string | undefined = undefined,
+  bearer: string | undefined = undefined
+): Promise<User> => {
+  let route: string = key ? `${API}/user/${key}` : `${API}`;
 
-  const response = await fetch(route, {
+  let options: RequestInit = {
     method: 'GET',
     credentials: 'include'
-  });
+  };
+  if (bearer) {
+    options.headers = {
+      Authorization: `Bearer ${bearer}`
+    };
+  }
 
-  const data: User[] = await response.json();
+  const response = await fetch(route, options);
+
+  const data: User = await response.json();
   if (!data) {
     throw new NotFoundError(`Could not find user with ID ${key}.`);
   }
-  return data[0];
+  return data;
 };
 
 export default getUser;
