289b520d45b6a85f439fc4bc56cd29e3ad1eef2b
bdbaraban <375@holbertonschool.com>
2019-09-08 12:22:45 -0700

    Load import, react-hooks, jest and jsx-a11y plugins

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,21 +5,96 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended'
   ],
-  'plugins': ['react', '@typescript-eslint', 'prettier'],
-  'env': {
-    'browser': true,
-    'jasmine': true,
-    'jest': true
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'react-hooks',
+    'prettier',
+    'import',
+    'jsx-a11y',
+    'jest'
+  ],
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+    es6: true
   },
-  'rules': {
-    'prettier/prettier': ['error', { 'singleQuote': true }]
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    '@typescript-eslint/camelcase': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
-  'settings': {
-    'react': {
-      'pragma': 'React',
-      'version': 'detect'
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect'
+    },
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
     }
   },
-}
+  overrides: [
+    {
+      files: ['server/authFunctions.ts'],
+      rules: {
+        '@typescript-eslint/ban-ts-ignore': 'off'
+      }
+    },
+    {
+      files: ['server/authRoutes.ts'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off'
+      }
+    },
+    {
+      files: ['pages/_app.tsx'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off'
+      }
+    },
+    {
+      files: ['pages/_document.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off'
+      }
+    },
+    {
+      files: ['next.config.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off'
+      }
+    },
+    {
+      files: ['src/components/TagsSelect.tsx', 'src/components/Link.tsx'],
+      rules: {
+        'react/display-name': 'off'
+      }
+    },
+    {
+      files: ['src/components/Link.tsx'],
+      rules: {
+        'jsx-a11y/anchor-has-content': 'off'
+      }
+    },
+    {
+      files: ['src/store/tags/reducer.ts'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
+  ]
+};
