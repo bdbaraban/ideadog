9a69706cb4c3d9e0a0e01ef1a1ebc823cd4b7450
bdbaraban <375@holbertonschool.com>
2019-09-08 12:22:12 -0700

    Upload Jest configuration file

---
+++ frontend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleDirectories: ['node_modules', 'src', __dirname],
+  cacheDirectory: '.jest/cache'
+};
