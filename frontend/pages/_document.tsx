566dec3ce4201b06f76e4776f80cfb138403bcbf
bdbaraban <375@holbertonschool.com>
2019-09-08 12:32:26 -0700

    Upload customized Next.js document that loads Material-UI CSS

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,66 @@
+import React, { Fragment, ReactElement } from 'react';
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Main,
+  NextScript
+} from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
+import theme from 'theme';
+
+/**
+ * Load Material-UI styles for rendered HTML
+ */
+export default class CustomDocument extends Document {
+  static getInitialProps = async (
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> => {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />)
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        <Fragment key="styles">
+          {initialProps.styles}
+          {sheets.getStyleElement()}
+        </Fragment>
+      ]
+    };
+  };
+
+  render(): ReactElement {
+    return (
+      <html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Lato&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
