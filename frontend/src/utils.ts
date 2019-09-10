aa2e902298a71cfbaa6c55d54e25aee46fae195c
bdbaraban <375@holbertonschool.com>
2019-09-10 11:39:15 -0700

    Delete old getAPI and getHostname functions and condense remaining utilities into single utils.ts file

---
+++ frontend/src/utils.ts
@@ -0,0 +1,46 @@
+// Constant months codex
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+/**
+ * Format a date type like <full month name> <day>, <year>.
+ * @param date {Date} - The date type to format.
+ */
+export const formatLongDate = (date: Date): string => {
+  return `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+};
+
+/**
+ * Format a date type like <month number>/<day>/<year>.
+ * @param date {Date} - The date type to format.
+ */
+export const formatShortDate = (date: Date): string => {
+  return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+};
+
+/**
+ * Split and capitalize a tag name.
+ * @param tag {string} - The tag name to format.
+ */
+export const formatTag = (tag: string): string => {
+  return tag
+    .split(new RegExp('[_ ]', 'g'))
+    .reduce(
+      (formatted: string, word: string): string =>
+        formatted + `${word[0].toUpperCase()}${word.substring(1)} `,
+      ''
+    )
+    .trimEnd();
+};
--- frontend/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-// utils barrel file
-export { default as calculateBrightness } from './calculateBrightness';
-export { default as formatDate } from './formatDate';
-export { default as formatTag } from './formatTag';
-export { default as getAPI } from './getAPI';
-export { default as getHostname } from './getHostname';
-export { default as setWithoutValue } from './setWithoutValue';
