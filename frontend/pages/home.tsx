250aa841e8ca18b7040ab2daedcd694c6c0a326c
bdbaraban <375@holbertonschool.com>
2019-09-10 11:50:32 -0700

    Fetch initial ideas and tags within home page getInitialProps

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,15 +1,44 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
+import { NextPage, NextPageContext } from 'next';
 import { IdeaFeed, Layout } from 'components';
+import { AppStore } from 'store';
+import { fetchIdeas } from 'store/ideas/actions';
+import { fetchTags } from 'store/tags/actions';
+
+// Home page props
+interface HomeProps {
+  error?: string; // Authorization error message
+}
 
 /**
  * Main home page
  */
-const Home = (): ReactElement => {
+const Home: NextPage<HomeProps> = ({ error }: HomeProps): ReactElement => {
+  useEffect((): void => {
+    if (error) {
+      alert(`Login Error: ${error}`);
+    }
+  }, [error]);
+
   return (
     <Layout>
       <IdeaFeed />
     </Layout>
   );
+};
+
+Home.getInitialProps = async ({
+  query,
+  store
+}: NextPageContext & {
+  store: AppStore;
+}): Promise<HomeProps> => {
+  // Fetch ideas
+  await store.dispatch(fetchIdeas());
+  // Fetch tags
+  await store.dispatch(fetchTags());
+
+  return { error: query.error as string };
 };
 
 export default Home;
