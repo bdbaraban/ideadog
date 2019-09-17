d7f7b7746ef8660abf2e7dc52eb791c8efb86210
bdbaraban <375@holbertonschool.com>
2019-09-16 21:59:53 -0700

    Clean up organization of tests with describe blocks for each utility function

--- frontend/__tests__/utils.test.ts
+++ frontend/__tests__/utils.test.ts
@@ -1,163 +1,219 @@
 import { formatLongDate, formatShortDate, formatTag } from 'utils';
 
-test('formatLongDate correctly formats all day/month combinations', (): void => {
-  // January
-  for (let day = 1; day <= 31; day++) {
-    expect(formatLongDate(new Date(`1-${day}-2019`))).toEqual(
-      `January ${day}, 2019`
-    );
-  }
+describe('formatLongDate', (): void => {
+  test('January', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatLongDate(new Date(`1-${day}-2019`))).toEqual(
+        `January ${day}, 2019`
+      );
+    }
+  });
 
-  // February
-  for (let day = 1; day <= 29; day++) {
-    expect(formatLongDate(new Date(`2-${day}-2016`))).toEqual(
-      `February ${day}, 2016`
-    );
-  }
+  test('February', (): void => {
+    for (let day = 1; day <= 29; day++) {
+      expect(formatLongDate(new Date(`2-${day}-2016`))).toEqual(
+        `February ${day}, 2016`
+      );
+    }
+  });
 
-  // March
-  for (let day = 1; day <= 31; day++) {
-    expect(formatLongDate(new Date(`3-${day}-2019`))).toEqual(
-      `March ${day}, 2019`
-    );
-  }
+  test('March', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatLongDate(new Date(`3-${day}-2019`))).toEqual(
+        `March ${day}, 2019`
+      );
+    }
+  });
 
-  // April
-  for (let day = 1; day <= 30; day++) {
-    expect(formatLongDate(new Date(`4-${day}-2019`))).toEqual(
-      `April ${day}, 2019`
-    );
-  }
+  test('April', (): void => {
+    for (let day = 1; day <= 30; day++) {
+      expect(formatLongDate(new Date(`4-${day}-2019`))).toEqual(
+        `April ${day}, 2019`
+      );
+    }
+  });
 
-  // May
-  for (let day = 1; day <= 31; day++) {
-    expect(formatLongDate(new Date(`5-${day}-2019`))).toEqual(
-      `May ${day}, 2019`
-    );
-  }
+  test('May', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatLongDate(new Date(`5-${day}-2019`))).toEqual(
+        `May ${day}, 2019`
+      );
+    }
+  });
 
-  // June
-  for (let day = 30; day <= 30; day++) {
-    expect(formatLongDate(new Date(`6-${day}-2019`))).toEqual(
-      `June ${day}, 2019`
-    );
-  }
+  test('June', (): void => {
+    for (let day = 30; day <= 30; day++) {
+      expect(formatLongDate(new Date(`6-${day}-2019`))).toEqual(
+        `June ${day}, 2019`
+      );
+    }
+  });
 
-  // July
-  for (let day = 1; day <= 31; day++) {
-    expect(formatLongDate(new Date(`7-${day}-2019`))).toEqual(
-      `July ${day}, 2019`
-    );
-  }
+  test('July', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatLongDate(new Date(`7-${day}-2019`))).toEqual(
+        `July ${day}, 2019`
+      );
+    }
+  });
 
-  // August
-  for (let day = 1; day <= 31; day++) {
-    expect(formatLongDate(new Date(`8-${day}-2019`))).toEqual(
-      `August ${day}, 2019`
-    );
-  }
+  test('August', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatLongDate(new Date(`8-${day}-2019`))).toEqual(
+        `August ${day}, 2019`
+      );
+    }
+  });
 
-  // September
-  for (let day = 1; day <= 30; day++) {
-    expect(formatLongDate(new Date(`9-${day}-2019`))).toEqual(
-      `September ${day}, 2019`
-    );
-  }
+  test('September', (): void => {
+    for (let day = 1; day <= 30; day++) {
+      expect(formatLongDate(new Date(`9-${day}-2019`))).toEqual(
+        `September ${day}, 2019`
+      );
+    }
+  });
 
-  // October
-  for (let day = 1; day <= 31; day++) {
-    expect(formatLongDate(new Date(`10-${day}-2019`))).toEqual(
-      `October ${day}, 2019`
-    );
-  }
+  test('October', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatLongDate(new Date(`10-${day}-2019`))).toEqual(
+        `October ${day}, 2019`
+      );
+    }
+  });
 
-  // November
-  for (let day = 1; day <= 30; day++) {
-    expect(formatLongDate(new Date(`11-${day}-2019`))).toEqual(
-      `November ${day}, 2019`
-    );
-  }
+  test('November', (): void => {
+    for (let day = 1; day <= 30; day++) {
+      expect(formatLongDate(new Date(`11-${day}-2019`))).toEqual(
+        `November ${day}, 2019`
+      );
+    }
+  });
 
-  // December
-  for (let day = 1; day <= 31; day++) {
-    expect(formatLongDate(new Date(`12-${day}-2019`))).toEqual(
-      `December ${day}, 2019`
-    );
-  }
+  test('December', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatLongDate(new Date(`12-${day}-2019`))).toEqual(
+        `December ${day}, 2019`
+      );
+    }
+  });
 });
 
-test('formatShortDate correctly formats all day/month combinations', (): void => {
-  // January
-  for (let day = 1; day <= 31; day++) {
-    expect(formatShortDate(new Date(`1-${day}-2019`))).toEqual(`1/${day}/2019`);
-  }
+describe('formatShortDate', (): void => {
+  test('January', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatShortDate(new Date(`1-${day}-2019`))).toEqual(
+        `1/${day}/2019`
+      );
+    }
+  });
 
-  // February
-  for (let day = 1; day <= 29; day++) {
-    expect(formatShortDate(new Date(`2-${day}-2016`))).toEqual(`2/${day}/2016`);
-  }
+  test('February', (): void => {
+    for (let day = 1; day <= 29; day++) {
+      expect(formatShortDate(new Date(`2-${day}-2016`))).toEqual(
+        `2/${day}/2016`
+      );
+    }
+  });
 
-  // March
-  for (let day = 1; day <= 31; day++) {
-    expect(formatShortDate(new Date(`3-${day}-2019`))).toEqual(`3/${day}/2019`);
-  }
+  test('March', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatShortDate(new Date(`3-${day}-2019`))).toEqual(
+        `3/${day}/2019`
+      );
+    }
+  });
 
-  // April
-  for (let day = 1; day <= 30; day++) {
-    expect(formatShortDate(new Date(`4-${day}-2019`))).toEqual(`4/${day}/2019`);
-  }
+  test('April', (): void => {
+    for (let day = 1; day <= 30; day++) {
+      expect(formatShortDate(new Date(`4-${day}-2019`))).toEqual(
+        `4/${day}/2019`
+      );
+    }
+  });
 
-  // May
-  for (let day = 1; day <= 31; day++) {
-    expect(formatShortDate(new Date(`5-${day}-2019`))).toEqual(`5/${day}/2019`);
-  }
+  test('May', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatShortDate(new Date(`5-${day}-2019`))).toEqual(
+        `5/${day}/2019`
+      );
+    }
+  });
 
-  // June
-  for (let day = 30; day <= 30; day++) {
-    expect(formatShortDate(new Date(`6-${day}-2019`))).toEqual(`6/${day}/2019`);
-  }
+  test('June', (): void => {
+    for (let day = 30; day <= 30; day++) {
+      expect(formatShortDate(new Date(`6-${day}-2019`))).toEqual(
+        `6/${day}/2019`
+      );
+    }
+  });
 
-  // July
-  for (let day = 1; day <= 31; day++) {
-    expect(formatShortDate(new Date(`7-${day}-2019`))).toEqual(`7/${day}/2019`);
-  }
+  test('July', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatShortDate(new Date(`7-${day}-2019`))).toEqual(
+        `7/${day}/2019`
+      );
+    }
+  });
 
-  // August
-  for (let day = 1; day <= 31; day++) {
-    expect(formatShortDate(new Date(`8-${day}-2019`))).toEqual(`8/${day}/2019`);
-  }
+  test('August', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatShortDate(new Date(`8-${day}-2019`))).toEqual(
+        `8/${day}/2019`
+      );
+    }
+  });
 
-  // September
-  for (let day = 1; day <= 30; day++) {
-    expect(formatShortDate(new Date(`9-${day}-2019`))).toEqual(`9/${day}/2019`);
-  }
+  test('September', (): void => {
+    for (let day = 1; day <= 30; day++) {
+      expect(formatShortDate(new Date(`9-${day}-2019`))).toEqual(
+        `9/${day}/2019`
+      );
+    }
+  });
 
-  // October
-  for (let day = 1; day <= 31; day++) {
-    expect(formatShortDate(new Date(`10-${day}-2019`))).toEqual(
-      `10/${day}/2019`
-    );
-  }
+  test('October', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatShortDate(new Date(`10-${day}-2019`))).toEqual(
+        `10/${day}/2019`
+      );
+    }
+  });
 
-  // November
-  for (let day = 1; day <= 30; day++) {
-    expect(formatShortDate(new Date(`11-${day}-2019`))).toEqual(
-      `11/${day}/2019`
-    );
-  }
+  test('November', (): void => {
+    for (let day = 1; day <= 30; day++) {
+      expect(formatShortDate(new Date(`11-${day}-2019`))).toEqual(
+        `11/${day}/2019`
+      );
+    }
+  });
 
-  // December
-  for (let day = 1; day <= 31; day++) {
-    expect(formatShortDate(new Date(`12-${day}-2019`))).toEqual(
-      `12/${day}/2019`
-    );
-  }
+  test('December', (): void => {
+    for (let day = 1; day <= 31; day++) {
+      expect(formatShortDate(new Date(`12-${day}-2019`))).toEqual(
+        `12/${day}/2019`
+      );
+    }
+  });
 });
 
-test('formatTag correctly handles tag combinations', (): void => {
-  expect(formatTag('animals')).toEqual('Animals');
-  expect(formatTag('video_game')).toEqual('Video Game');
-  expect(formatTag('board game')).toEqual('Board Game');
-  expect(formatTag('Dogs')).toEqual('Dogs');
-  expect(formatTag('dogs_and_Cats')).toEqual('Dogs And Cats');
+describe('formatTag', (): void => {
+  test('one word lowercase', (): void => {
+    expect(formatTag('animals')).toEqual('Animals');
+  });
+
+  test('one word uppercase', (): void => {
+    expect(formatTag('Animals')).toEqual('Animals');
+  });
+
+  test('two words separated by underscore', (): void => {
+    expect(formatTag('video_game')).toEqual('Video Game');
+  });
+
+  test('two words separated by space', (): void => {
+    expect(formatTag('board game')).toEqual('Board Game');
+  });
+
+  test('three words separated by underscore and space', (): void => {
+    expect(formatTag('dogs_and cats')).toEqual('Dogs And Cats');
+  });
 });
