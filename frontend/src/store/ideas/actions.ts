d8d382ffe459fe972d9a56785fb29babf0afddf9
bdbaraban <375@holbertonschool.com>
2019-09-16 21:57:53 -0700

    Detect fetch errors with try/except instead of null-checking data response

--- frontend/src/store/ideas/actions.ts
+++ frontend/src/store/ideas/actions.ts
@@ -1,95 +1,107 @@
 import { Action } from 'redux';
-import { ThunkAction } from 'redux-thunk';
-import fetch from 'isomorphic-unfetch';
-import { AppState } from 'store';
+import 'isomorphic-unfetch';
+
+import { ActionType } from 'store';
 import {
   FETCH_IDEA_FAILURE,
-  FETCH_IDEAS_FAILURE,
   FETCH_IDEA_SUCCESS,
+  FETCH_IDEAS_FAILURE,
   FETCH_IDEAS_SUCCESS,
-  FETCH_USER_IDEAS_SUCCESS,
-  FETCH_USER_IDEAS_FAILURE
-} from 'store/ideas/types';
+  FETCH_USER_IDEAS_FAILURE,
+  FETCH_USER_IDEAS_SUCCESS
+} from './types';
 
 /**
  * Fetch ideas from the IdeaDog API.
+ * @param sort - Sorting filter (`all` or `bright`).
+ * @param search - Search query.
+ * @param tags - Tags filter (comma-separated string).
  */
 export const fetchIdeas = (
   sort = 'all',
   search = '',
   tags = ''
-): ThunkAction<void, AppState, null, Action<string>> => async (
-  dispatch
-): Promise<Action<string>> => {
+): ActionType => async (dispatch): Promise<Action<string>> => {
   let query = `${process.env.IDEADOG_API}/ideas`;
-  query += sort !== 'all' ? `/${encodeURI(sort)}` : '';
-  query += search !== '' ? `?q=${encodeURI(search)}` : '';
-  query += tags !== '' ? `?tags=${encodeURI(tags)}` : '';
-  console.log('Fetching ideas at', query);
-  const response = await fetch(query);
+  query += sort !== 'all' ? `/${encodeURIComponent(sort)}` : '';
+  query += search !== '' ? `?q=${encodeURIComponent(search)}` : '';
+  query +=
+    tags !== ''
+      ? search === ''
+        ? `?tags=${encodeURIComponent(tags)}`
+        : `&tags=${encodeURIComponent(tags)}`
+      : '';
 
-  const data = await response.json();
+  try {
+    const response = await fetch(query);
+    const data = await response.json();
 
-  if (!data) {
+    return dispatch({
+      type: FETCH_IDEAS_SUCCESS,
+      payload: data
+    });
+  } catch {
     return dispatch({
       type: FETCH_IDEAS_FAILURE,
       payload: 'Could not fetch ideas - please try again later.'
     });
   }
-  return dispatch({
-    type: FETCH_IDEAS_SUCCESS,
-    payload: data
-  });
 };
 
 /**
  * Fetch a single idea from the IdeaDog API.
+ * @param key - Key of idea to fetch.
  */
-export const fetchIdea = (
-  key = ''
-): ThunkAction<void, AppState, null, Action<string>> => async (
+export const fetchIdea = (key = ''): ActionType => async (
   dispatch
 ): Promise<Action<string>> => {
   const query = `${process.env.IDEADOG_API}/idea/${key}`;
-  console.log('Fetching idea at', query);
-  const response = await fetch(query);
 
-  const data = await response.json();
+  try {
+    const response = await fetch(query);
+    const data = await response.json();
 
-  if (!data) {
+    if (data.length === 0) {
+      throw new Error();
+    }
+
+    return dispatch({
+      type: FETCH_IDEA_SUCCESS,
+      payload: data
+    });
+  } catch {
     return dispatch({
       type: FETCH_IDEA_FAILURE,
       payload: `Idea with ID ${key} does not exist.`
     });
   }
-  return dispatch({
-    type: FETCH_IDEA_SUCCESS,
-    payload: data
-  });
 };
 
 /**
- * Fetch ideas for a given user from the IdeaDog API.
+ * Fetch ideas posted by a given user from the IdeaDog API.
+ * @param key - Key of user for which to fetch ideas.
  */
-export const fetchUserIdeas = (
-  key = ''
-): ThunkAction<void, AppState, null, Action<string>> => async (
+export const fetchUserIdeas = (key = ''): ActionType => async (
   dispatch
 ): Promise<Action<string>> => {
   const query = `${process.env.IDEADOG_API}/user/${key}/ideas`;
-  console.log('Fetching user ideas at', query);
-  const response = await fetch(query);
 
-  const data = await response.json();
+  try {
+    const response = await fetch(query);
+    const data = await response.json();
 
-  if (!data) {
+    if (data.length === 0) {
+      throw new Error();
+    }
+
+    return dispatch({
+      type: FETCH_USER_IDEAS_SUCCESS,
+      payload: data
+    });
+  } catch {
     return dispatch({
       type: FETCH_USER_IDEAS_FAILURE,
       payload: `User with ID ${key} does not exist.`
     });
   }
-  return dispatch({
-    type: FETCH_USER_IDEAS_SUCCESS,
-    payload: data
-  });
 };
